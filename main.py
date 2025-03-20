f674f4aef8a529d8f52c8470e2c89cde372c8100
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-03-20 12:32:38 +0530

    INIT: create main module for API

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Instantiate FastAPI application
+app = FastAPI(
+    title="TransferServiceAPI",
+    description="RESTful API for mocking transfers between accounts. Internship Programming Assignment - Group Technology at Dialog.",
+)
+
+
+# Define allowed origins for CORS
+origins = [
+    "http://localhost:3000",
+]
+# NOTE
+# Assumed that a client running on the above URL
+# will require connecting to this service.
+
+# Setup CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Root route (to test service health)
+@app.get("/", tags=["Internals"])
+async def root():
+    return {
+        "message": "TransferService is up and running.",
+    }
