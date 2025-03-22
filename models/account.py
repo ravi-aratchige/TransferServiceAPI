3e270398d698143cbb78250f180b5cbc7c9e0796
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-03-22 13:07:21 +0530

    CREATE: account model and models package

---
+++ models/account.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class AccountModel(BaseModel):
+    # Account number
+    account_number: str
+
+    # Account balance
+    balance: float = Field(
+        ge=0,
+        description="Account balance cannot be negative.",
+    )
