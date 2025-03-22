78a53280f66b6c927dc8ec660e3546ce4493b8ab
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-03-22 13:08:09 +0530

    CREATE: route to fetch all accounts

--- routes/accounts.py
+++ routes/accounts.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from database.accounts import AccountDataManager
+
 # Initialize accounts router
 router = APIRouter(
     prefix="/accounts",
@@ -16,4 +18,16 @@ router = APIRouter(
 def test_router():
     return {
         "message": "TransferServiceAPI Accounts router is up and running.",
+    }
+
+
+# Get all accounts
+@router.get("/")
+def get_all_accounts():
+    # Retrieve accounts from data source
+    accounts = AccountDataManager.load_accounts()
+
+    return {
+        "message": "Accounts retrieved successfully",
+        "data": accounts,
     }
