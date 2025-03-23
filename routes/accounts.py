b0be6727e390ef7e4ca8a9e9eea533c7d6db074f
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-03-23 10:27:53 +0530

    CREATE: route to retrieve a single account

--- routes/accounts.py
+++ routes/accounts.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
 from models.account import AccountModel
-from database.accounts import AccountDataManager
+from data.accounts import AccountDataManager
+from fastapi import APIRouter, status, HTTPException
 
 # Initialize accounts router
 router = APIRouter(
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 # Test router health
-@router.get("/ping")
+@router.get("/ping", status_code=status.HTTP_200_OK)
 def test_accounts_router():
     return {
         "message": "TransferServiceAPI Accounts router is up and running.",
@@ -22,7 +22,7 @@ def test_accounts_router():
 
 
 # Get all accounts
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK)
 def get_all_accounts():
     # Initialize account data manager
     data_manager = AccountDataManager()
@@ -36,8 +36,31 @@ def get_all_accounts():
     }
 
 
+# Get a single account by account_number
+@router.get("/{account_number}", status_code=status.HTTP_200_OK)
+def get_account(account_number: str):
+    """Retrieve a specific account by its account_number."""
+
+    # Initialize account data manager
+    data_manager = AccountDataManager()
+
+    # Retrieve all accounts
+    accounts = data_manager.read_all()
+
+    # Search for the specific account
+    for account in accounts:
+        if account.account_number == account_number:
+            return {
+                "message": "Account retrieved successfully",
+                "data": account,
+            }
+
+    # If not found, raise a 404 error
+    raise HTTPException(status_code=404, detail=f"Account {account_number} not found.")
+
+
 # Create new account
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_account(new_account: AccountModel):
     # Initialize account data manager
     data_manager = AccountDataManager()
